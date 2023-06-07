434f46497d314833f181a6e2972c024492ed00f0
osmany100785 <montenegroosmany87@gmail.com>
2023-06-06 20:05:45 -0600

    format code

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,20 +1,24 @@
-import {Link} from 'react-router-dom';
-import {Searchbar} from '../index'
-import style from "./NavBar.module.css"
+import { Link } from "react-router-dom";
+import { Searchbar } from "../index";
+import style from "./NavBar.module.css";
 
 const NavBar = () => {
-    return(
-        <div className={style.Navegation}>
-        <img alt="img" className={style.imgs} src="https://25.media.tumblr.com/a1e87d2030a73aee16661e8807da6c1d/tumblr_mkhnmmFwaA1rxvkeso1_500.gif"/>
-        <Link to='/home'>
+  return (
+    <div className={style.Navegation}>
+      <img
+        alt="img"
+        className={style.imgs}
+        src="https://25.media.tumblr.com/a1e87d2030a73aee16661e8807da6c1d/tumblr_mkhnmmFwaA1rxvkeso1_500.gif"
+      />
+      <Link to="/home">
         <p>Home</p>
-        </Link> 
-        <Link to='/form'>
+      </Link>
+      <Link to="/form">
         <p>Create Pokemon</p>
-        </Link> 
-        <Searchbar />
-        </div>
-    )
-}
+      </Link>
+      <Searchbar />
+    </div>
+  );
+};
 
 export default NavBar;
